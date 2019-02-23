8d883f9ff35fc4e6883ee87d6e559e82c6347d42
Igor Bueno <igor_mcb@hotmail.com>
2019-02-22 22:18:30 -0400

    Inclusão do Spinner de tecnologias e activity etapa 3

--- app/src/main/java/br/edu/ifro/querointernet/FormularioEtapa1Activity.java
+++ app/src/main/java/br/edu/ifro/querointernet/FormularioEtapa1Activity.java
@@ -4,11 +4,14 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.Spinner;
 
 public class FormularioEtapa1Activity extends AppCompatActivity {
 
     private Button avancarBtnEtapa1;
+    private Spinner spinnerTiposTecnologia;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +22,15 @@ public class FormularioEtapa1Activity extends AppCompatActivity {
     }
 
     private void initializeComponents() {
+        //Itens do Spinner
+        String[] tiposTecnologia = {"Via rádio", "Fibra óptica", "ADSL", "Internet móvel"};
+        spinnerTiposTecnologia = findViewById(R.id.activity_formulario_etapa_1_tipos_tecnologia);
+
+        ArrayAdapter<String> spinnerArrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, tiposTecnologia);
+        spinnerArrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinnerTiposTecnologia.setAdapter(spinnerArrayAdapter);
+
+        //Ação da botão avançar
         this.avancarBtnEtapa1 = findViewById(R.id.activity_formulario_etapa_1_avancar);
 
         this.avancarBtnEtapa1.setOnClickListener(new View.OnClickListener() {
